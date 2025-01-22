b41ffe9a678ddf076f8f93c7cd3fd73115765d75
victorpendleton <victor@indeed.com>
2025-01-22 06:59:38 -0600

    Evaluating base model

--- apply_lightweight_fine_tunning_to_a_fm.py
+++ apply_lightweight_fine_tunning_to_a_fm.py
@@ -5,11 +5,15 @@ from peft import (AutoPeftModelForSequenceClassification,
                   PeftType,
                   TaskType)
 from datasets import (load_dataset)
-from transformers import (DistilBertForSequenceClassification,
+from transformers import (AutoTokenizer,
+                          AutoModelForSequenceClassification,
+                          DistilBertForSequenceClassification,
                           DistilBertTokenizer,
+                          pipeline,
                           TrainingArguments,
                           Trainer
                           )
+from torch.nn.functional import softmax
 import evaluate
 import torch
 
@@ -17,6 +21,7 @@ import torch
 metric = evaluate.load("accuracy")
 # Define the model to use
 model_name = "distilbert-base-uncased"
+
 
 # Function to tokenize the data
 def tokenize_function(examples):
@@ -31,6 +36,31 @@ def compute_metrics(eval_pred):
     predictions = torch.argmax(torch.tensor(logits), dim=1)
     print("Predictions and labels:", predictions, labels)
     return metric.compute(predictions=predictions, references=labels)
+
+
+def evaluate_singleton(text_to_evaluate, model):
+    inputs = tokenizer(text_to_evaluate, return_tensors="pt", truncation=True, padding=True, max_length=512)
+    outputs = model(**inputs)
+    probs = softmax(outputs.logits, dim=-1).detach().numpy()
+
+    print("\nDetailed Evaluation on One Sample:")
+    print(f"Input Text: {text_to_evaluate[:200]}...\nProbabilities: {probs}\n")
+
+
+def evaluate_model(model, tokenizer, data_to_evaluate):
+    classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+    predictions = classifier(data_to_evaluate)
+
+    # Print the predictions
+    print("\nPredictions on Sample Texts:")
+    for text, prediction in zip(data_to_evaluate, predictions):
+        print(f"Text: {text[:100]}...\nPrediction: {prediction}\n")
+
+
+def view_dataset(dataset_to_view, rows=10):
+    print("Dataset Preview:")
+    for x in range(rows):
+        print(dataset_to_view[x])
 
 
 def train_and_evaluate_model(model_to_train_and_evaluate, tokenizer_to_user, train_ds, test_ds, num_epochs=1, learn_rate=2e-5):
@@ -72,17 +102,31 @@ test_subset = dataset["test"].train_test_split(test_size=0.01, seed=42)["test"]
 print("Training data:", train_subset)
 print("Testing data:", test_subset)
 
+# # View some of the data
+# view_dataset(train_subset, 10)
+
 # Create the model and tokenizer
 # Distilled Bert
-base_model = DistilBertForSequenceClassification.from_pretrained(
-    model_name, num_labels=2
-)
-tokenizer = DistilBertTokenizer.from_pretrained(model_name)
+# base_model = DistilBertForSequenceClassification.from_pretrained(
+#     model_name, num_labels=2
+# )
+# tokenizer = DistilBertTokenizer.from_pretrained(model_name)
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+base_model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
+
 
 if tokenizer.pad_token is None:
     tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 print("Model:", base_model)
 print("Tokenizer:", tokenizer)
+
+# Evaluate the model
+evaluate_model(base_model, tokenizer, test_subset['text'][:5])
+
+# Test single entry
+evaluate_singleton("I really, really, really hate it", base_model)
+evaluate_singleton("loved it!", base_model)
 
 # Tokenize the data
 # training data
@@ -103,7 +147,6 @@ lora_config = LoraConfig(
     task_type=TaskType.SEQ_CLS,
     bias="none"
 )
-
 
 # LoRA
 # LoRA model name
@@ -126,7 +169,7 @@ lora_model = AutoPeftModelForSequenceClassification.from_pretrained(lora_model_n
 
 print("LoRA Model:", lora_model)
 
-
+'''
 # Train and evaluate the models
 base_model_results = train_and_evaluate_model(base_model, tokenizer,
                                               tokenized_train_dataset, tokenized_test_dataset, 1,
@@ -137,3 +180,4 @@ lora_model_results = train_and_evaluate_model(lora_model, tokenizer, tokenized_t
 print("Base model results:", base_model_results)
 print("Fine-tuned model results:", lora_model_results)
 
+'''
